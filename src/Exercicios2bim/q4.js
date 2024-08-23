5cc4e585e51296a7a5879be31d6ea0ba14bf6eb5
afonsotrs <afonsotorrescp@gmail.com>
2024-08-23 15:59:06 -0300

    q4

---
+++ src/Exercicios2bim/q4.js
@@ -0,0 +1,16 @@
+// Questão 4: Escreva uma função chamada replaceWord que recebe três argumentos: uma
+// string original, uma palavra a ser substituída e uma palavra substituta. A função deve
+// retornar a string original com todas as ocorrências da palavra a ser substituída trocadas
+// pela palavra substituta. Por exemplo:
+// replaceWord("The quick brown fox jumps over the lazy dog", "fox", "cat");
+// // retorna "The quick brown cat jumps over the lazy dog"
+
+
+function replaceWord(){
+
+}
+
+
+const replaceWord = (string_original, string_substitida, string_substituta) => {
+
+}
